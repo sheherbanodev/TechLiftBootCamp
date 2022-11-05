4d418b5947c8c68eb289ae0f7eb98b1156b124a5
Sheher Bano <Sheherbano.ctn@gmail.com>
2022-11-06 00:54:20 +0500

    style

--- sheher-activity-6/utility/profile.js
+++ sheher-activity-6/utility/profile.js
@@ -1,24 +1,18 @@
-
 var userObj = null;
 
 async function apiCall() {
-  let response = await fetch("https://api.github.com/users/junaidsaleem");
+  let response = await fetch("https://api.github.com/users/sheherbanodev");
   let value = await response.json();
 
-    userObj = value;
-  document.getElementById('name').innerText=value.name
-  document.getElementById('img').src=value.avatar_url
-  document.getElementById('bio').innerText=value.bio
+  userObj = value;
+  document.getElementById("name").innerText = value.name;
+  document.getElementById("img").src = value.avatar_url;
+  document.getElementById("bio").innerText = value.bio;
 }
 
-function openProfile(){
-    window.open(
-        userObj.html_url,
-        '_blank' // <- This is what makes it open in a new window.
-      );
+function openProfile() {
+  window.open(
+    userObj.html_url,
+    "_blank" // <- This is what makes it open in a new window.
+  );
 }
-
-
-  
-
-
